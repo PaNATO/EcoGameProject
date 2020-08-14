8a03bb830e44aa6391338fe40684d00a32de5212
PaNATO <chief1097@gmail.com>
2020-08-14 06:39:19 +0200

    Ver-1.0.0
    +Created Scenes Script
    +Created MenuInterface Script(Changing scenes when clicked play)
    +Added Sprites Map(Poland, Czech,Slovakia)
    +Created first vision on Map in game
    +Some changes to camera

---
+++ EcoGameProject/Assets/Scripts/MenuUserInterface.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuUserInterface : MonoBehaviour
+{
+    public Transform Menu;
+    public static bool isGameRunning;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneNames.GamePlay);
+        isGameRunning = true;
+    }
+    public void MenuGame()
+    {
+        SceneManager.LoadScene(SceneNames.Menu);
+        isGameRunning = false;
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ EcoGameProject/Assets/Scripts/SceneNames.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneNames
+{
+    public const string Menu = "Menu";
+    public const string GamePlay = "GamePlay";
+}
